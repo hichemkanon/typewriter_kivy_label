1d455304f2f926a94f34172630d1c52f69f295d8
hichemkanon <53584970+hichemkanon@users.noreply.github.com>
2024-04-14 17:37:49 +0100

    Update typewriter_label.py

--- typewriter_label.py
+++ typewriter_label.py
@@ -1,10 +1,13 @@
+import time
 from kivy.app import App
 from kivy.uix.label import Label
 from kivy.clock import Clock
 from kivy.core.window import Window
-
+from kivy.properties import StringProperty
+import threading
 
 class TypewriterLabel(Label):
+    custom_text = StringProperty("")
     def __init__(self, cursor_size=30, 
         cursor_color='#3cff00',
         time_interval=.07,**kwargs):
@@ -21,32 +24,46 @@ class TypewriterLabel(Label):
         self.cursor_color = cursor_color
         self.padding = [10, 5]
         self.texto = ''
+        self.text = self.custom_text
         self.time_interval = time_interval
         self.bind(size=self.setter('text_size'))
+        self.should_stop = False
     ct = 0 
-    def start_typing(self, text):
-        self.text = ""
+    def start_typing(self, content):
+        self.custom_text = ""
         global ct
         #self.typing_animation = Animation(opacity=1, duration=0.1)
         #self.typing_animation.start(self)
         
-        def add_text(dt):
-            if self.text == text or self.texto == text:
-                self.typing_event.cancel()
-            else:                
-                self.texto = self.texto + text[len(self.texto)]
-                printed = f"[size={self.cursor_size}][b][color=000000]|[/color][/b][/size]" if len(self.texto) % 2 == 1 else f"[size={self.cursor_size}][b][color={self.cursor_color}]|[/color][/b][/size]"
-                self.text = self.texto + printed
+        def add_text():
+            while not self.should_stop:
+                if self.custom_text == content or self.texto == content:
+                    self.should_stop == True
+                else:                
+                    self.texto = self.texto + content[len(self.texto)]
+                    printed = f"[size={self.cursor_size}][b][color=000000]|[/color][/b][/size]" if len(self.texto) % 2 == 1 else f"[size={self.cursor_size}][b][color={self.cursor_color}]|[/color][/b][/size]"
+                    self.custom_text = self.texto + printed
+                    Clock.schedule_once(lambda x: self.update_text_label(self.custom_text), 0)
+                time.sleep(self.time_interval)
+                
            
 
-        self.typing_event = Clock.schedule_interval(add_text, self.time_interval)
+        threading.Thread(target=add_text).start()
 
+    def update_text_label(self, text: str):
+        self.text = text
 
 class TypewriterApp(App):
     def build(self):
-        label = TypewriterLabel()
-        label.start_typing("def clone_widget(widget): \n    clone = type(widget)()  # Create a new instance of the same class\n    clone.__dict__.update(widget.__dict__)  # Copy properties from the original widget\n    return clone")
-        return label
+        self.label = TypewriterLabel()
+        self.label.start_typing("def clone_widget(widget): \n    clone = type(widget)()  # Create a new instance of the same class\n    clone.__dict__.update(widget.__dict__)  # Copy properties from the original widget\n    return clone")
+        return self.label
+    
+    
+    def on_stop(self, *args):
+        self.label.should_stop = True
+    
+    
 
 if __name__ == '__main__':
     TypewriterApp().run()
